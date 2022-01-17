72b8fad115c41ea3f341b9a81e8ab87e9465fac3
Gustavo Scabuzzi Gonçalves <34173963+GustavoScabuzzi@users.noreply.github.com>
2022-01-16 23:30:06 -0300

    [Feature Gustavo - Task: Setar as informacoes para o JavaBeans]

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -7,11 +7,13 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import model.DAO;
+import model.JavaBeans;
 
-@WebServlet(urlPatterns = {"/Controller", "/main"})
+@WebServlet(urlPatterns = {"/Controller", "/main", "/insert"})
 public class Controller extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        DAO dao = new DAO();
+       JavaBeans contato = new JavaBeans();
     
     public Controller() {
         super();
@@ -23,6 +25,10 @@ public class Controller extends HttpServlet {
 		
 		if(action.equals("/main")) {
 			contatos(request, response);
+		}else if(action.equals("/insert")) {
+			novoContato(request, response);
+		}else {
+			response.sendRedirect("index.html");
 		}
 		
 		// Teste de conexao
@@ -33,5 +39,33 @@ public class Controller extends HttpServlet {
 	protected void contatos(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.sendRedirect("agenda.jsp");
 	}
+	
+	// Listar contatos
+	protected void novoContato(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// Teste de recebimento dos dados do formulario
+//		System.out.println(request.getParameter("nome"));
+//		System.out.println(request.getParameter("fone"));
+//		System.out.println(request.getParameter("email"));
+		
+		//Setar as variaveis JavaBeans
+		contato.setNome(request.getParameter("nome"));
+		contato.setFone(request.getParameter("fone"));
+		contato.setEmail(request.getParameter("email"));
+	}
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
